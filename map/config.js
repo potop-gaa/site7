3958bd5a77e74870b9375431c7adb70e1b0d2ed6
potop-gaa <polmikon@gmail.com>
2025-04-26 01:59:15 +0300

    Create config.js

---
+++ map/config.js
@@ -0,0 +1,14 @@
+const ROOM_SIZES = {
+    'very-large': { width: 20, depth: 20, height: 5 },
+    'large': { width: 15, depth: 15, height: 5 },
+    'medium': { width: 10, depth: 10, height: 5 },
+    'mediumD': { width: 30, depth: 10, height: 5 },
+    'small': { width: 7, depth: 7, height: 5 },
+    'hall-straight': { width: 5, depth: 10, height: 3 },
+    'hall-corner': { width: 10, depth: 10, height: 3 },
+    'hall-t': { width: 15, depth: 15, height: 3 },
+    'hall-x': { width: 20, depth: 20, height: 3 }
+};
+
+const LCZ_MAP = [...]; // оригинальный массив lczMap
+const HCZ_MAP = [...]; // оригинальный массив hczMap
